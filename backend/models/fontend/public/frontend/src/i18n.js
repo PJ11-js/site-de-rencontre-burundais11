6eedc87cf91ba6c05c1cb3cf864ccad280b3b4ed
PJ11-js <pierrengendakuriyo5@gmail.com>
2025-07-19 12:59:11 +0200

    Create i18n.js

---
+++ backend/models/fontend/public/frontend/src/i18n.js
@@ -0,0 +1,42 @@
+const translations = {
+  fr: {
+    title: "Burundi Rencontre",
+    signup: "Inscription",
+    name: "Nom",
+    age: "Âge",
+    pic: "Photo",
+    signupBtn: "S’inscrire",
+    match: "Profils",
+    like: "Aimer",
+    dislike: "Ignorer",
+    chat: "Chat",
+    send: "Envoyer"
+  },
+  en: {
+    title: "Burundi Dating",
+    signup: "Sign Up",
+    name: "Name",
+    age: "Age",
+    pic: "Photo",
+    signupBtn: "Sign up",
+    match: "Profiles",
+    like: "Like",
+    dislike: "Pass",
+    chat: "Chat",
+    send: "Send"
+  },
+  rn: {
+    title: "Uhuriro w’urukundo Burundi",
+    signup: "Kwiyandikisha",
+    name: "Izina",
+    age: "Imyaka",
+    pic: "Ifoto",
+    signupBtn: "Iyandikishe",
+    match: "Abarundi",
+    like: "Kunda",
+    dislike: "Reka",
+    chat: "Ubutumwa",
+    send: "Rungika"
+  }
+};
+export default translations;
